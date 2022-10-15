f43fc91dd2734032fb1d551c8a791c8eaf2977fa
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-15 12:41:29 +0200

    Delete index.js

--- src/helpers/index.js
+++
@@ -1,8 +0,0 @@
-export const countriesApi = async () => {
-  const res = await fetch(
-    "https://ih-countries-api.herokuapp.com/countries"
-  ).then((res) => res.json());
-  return res;
-};
-
-//console.log(results);
