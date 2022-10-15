5021149aca12df72238141dccf7a2e474bca9841
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-15 16:35:15 +0200

    build

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: "/",
+    path: "/lab-vue-wiki-countries/",
     name: "countries",
     component: () => import("../views/CountriesList.vue"),
     children: [
